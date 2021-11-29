30d1df3433886285f1a4290583f660baa62d8fb1
Joey Hodson <joeyhodson32@gmail.com>
2021-11-28 22:02:41 -0500

    Finalized with output matching validation runs.

--- branch_predictor/branch.c
+++ branch_predictor/branch.c
@@ -11,6 +11,9 @@
 
     command line:
 
+    gcc ./branch.c -o ./main.out                        -----> this compiles
+    ./main.out 5 4 ./trace_files/mcf_trace.txt          -----> this executes
+
     /sim gshare <GPB> <RB> <Trace_File>
     1) GPB (M) = The number of PC bits used to index the gshare table.
     2) RB (N) = The global history register bits used to index the gshare table.
@@ -50,17 +53,23 @@ void initializeEntryTable(int* entryTable, int M) {
 }
 
 
-void output(FILE* outFile, int M, int N, double mispredictionRate) {
+void output(FILE* outFile, char* traceFile, int M, int N, double mispredictionRate) {
 
+    fprintf(outFile, "Trace file: %s\n", traceFile);
     fprintf(outFile, "Configuration: M = %d, N = %d\n", M, N);
-    fprintf(outFile, "Misprediction Rate: %.2f\%\n", 100*mispredictionRate);
+    fprintf(outFile, "Misprediction Rate: %.2f%%\n", 100*mispredictionRate);
+
+    printf("Trace file: %s\n", traceFile);
+    printf("Configuration: M = %d, N = %d\n", M, N);
+    printf("Misprediction Rate: %.2f%%\n", 100*mispredictionRate);
 }
 
-// TODO handle when M < N
+
 long long int getEntryTableIndex(long long int address, int* globalHistoryRegister, int M, int N) {
 
     unsigned int mask = (1 << M) - 1;
     long long int valueOfMBitField = (address >> BYTE_OFFSET_BIT_COUNT) & mask;
+
     long long int shiftedGHR = (*globalHistoryRegister << (M - N));
 
     long long int index = (valueOfMBitField ^ shiftedGHR);
@@ -145,7 +154,7 @@ double calculateMispredictionRate(FILE* inFile, int* entryTable, int M, int N) {
         totalPredictions++;
 
         if (printVerbose) {
-            printf("address=%llx, index=%d, M=%d, N=%d\n", address, entryTableIndex, M, N);
+            printf("address=%llx, index=%llx, M=%d, N=%d\n", address, entryTableIndex, M, N);
             printf("actual_path=%c, current_prediction=%d, mispredictions=%d\n\n", actualPath, entryTable[entryTableIndex], mispredictions);
         }
     }
@@ -168,7 +177,7 @@ int driver(int M, int N, char* traceFile) {
 
     double mispredictionRate = calculateMispredictionRate(inFile, entryTable, M, N);
 
-    output(outFile, M, N, mispredictionRate);
+    output(outFile, traceFile, M, N, mispredictionRate);
 
     free(entryTable);
 
@@ -194,6 +203,11 @@ int main(int argc, char *argv[]) {
     
     char* traceFile = (char*)malloc(100*sizeof(char));
     strcpy(traceFile, argv[3]);
+
+    if (N > M) {
+        printf("N can never be greater than M.\n");
+        return -1;
+    }
 
     return driver(M, N, traceFile);
 }
